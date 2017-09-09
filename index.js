51dcd75ba411a73a5e5f20492fce51e6d28f83f2
amanecer2 <sh2le86@gmail.com>
2017-09-09 17:25:38 +0300

    add socket io to server.js

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
 const port = process.env.PORT || 3000;
+const io = require('socket.io')(server);
 
 app.get('/', function (req, res) {
+
     res.send('Hello World!')
 });
+app.get('/me', function (req, res) {
+    res.send('Hello meeeeeeeeeee!')
+});
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+server.listen(port, function () {
+   console.log(`Example app listening on port ${port}!`)
+});
+
+io.on('connection', function(client) {
+    console.log(`Client ${client.id} connected...`);
+
+    io.emit('user joined',client.id);
+    client.on('join', function(data) {
+        console.log(data);
+    });
+    client.on('disconnected', function(data) {
+        console.log(`Client ${client.id} left...`);
+    });
+
 });
 
 module.exports = app;
